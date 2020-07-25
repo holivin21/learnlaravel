6465870314963b7e4e93e68e5a342c8633caa089
andres holivin <andres12holivin@gmail.com>
2020-07-25 23:59:40 +0700

    day4

--- app/Http/Controllers/post.php
+++ app/Http/Controllers/post.php
@@ -9,7 +9,7 @@ class post extends Controller
     public function show(PostModel $post){
         //firstOrFail()--> jika $post adalah null maka akan masuk ke halam 404
         //$post = /DB::table('posts)->where('SLUG',$slug)->firstOrFail(); --> ini digunakan ketika tidak ada model
-        // $post = PostModel::where('SLUG',$slug)->firstOrFail();
+        // $post = PostModel::where('SLUG',$post)->firstOrFail();
         return view('post/show',compact('post'));
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,33 @@
 @extends('navigation/master')
 @section('title','Login')
+@section('style')
+    <style>
+        #username{
+            text-align: left;
+        }
+    </style>
+@endsection
 @section('body')
+    <div class="container" >
+        <div class="form-body">
+            <form action="">
+                <div id="Username" class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" id="txtUsername" placeholder="Username">
+                </div>
+                <div id="Password" class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control" id="txtPassword" placeholder="Password">
+                </div>
+                <input type="checkbox" id="remember">Remember me
+                <div id="btn">
+                    <input type="submit" value="Login" class="btn btn-success">
+                    <input type="submit" value="Sign up" class="btn btn-info">
+                </div>
+                <span>Forget <a href="">Password?</a></span>
 
-    <div class="container">
-        <h1>Login</h1>
+            </form>
+        </div>
+
     </div>
-
 @endsection
